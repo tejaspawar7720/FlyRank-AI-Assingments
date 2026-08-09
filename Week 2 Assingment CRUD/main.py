d01d2a558b8e12797181d411e413428e8ed9706c
unknown <Tejas Pawar>
2026-08-09 16:39:27 +0530

    stage 2 done : read endpoints with 404 error code

--- Week 2 Assingment CRUD/main.py
+++ Week 2 Assingment CRUD/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from fastapi import FastAPI, HTTPException
 app = FastAPI()
 
 @app.get("/")
@@ -10,6 +10,28 @@ def root():
         "endpoints": ["/tasks"]
     }
 
+#to check if the API is running
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+#tasks data 
+tasks = [
+    {"id": 1, "title": "Buy groceries", "done": False},
+    {"id": 2, "title": "Read a book", "done": False},
+    {"id": 3, "title": "Go for a walk", "done": True}
+]
+
+#to get all tasks
+@app.get("/tasks")
+def get_all_tasks():
+    return tasks
+
+
+#task id to get a specific task
+@app.get("/tasks/{id}")
+def get_task(id: int):
+    for task in tasks:
+        if task["id"] == id:
+            return task
+    raise HTTPException(status_code=404, detail=f"Task {id} not found")
